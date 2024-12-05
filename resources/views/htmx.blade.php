57481ac59a3b522f8c4c79246cdf74094ad277e7
Lazarus Morrison <lazarusm@gmail.com>
2024-12-05 14:59:52 -0500

    02 - Copy code to clipboard

--- resources/views/htmx.blade.php
+++ resources/views/htmx.blade.php
@@ -5,13 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>hype cp | Hypermedia Copy & Paste</title>
 
-    <meta http-equiv="refresh" content="5">
+    <!-- <meta http-equiv="refresh" content="5"> -->
 
     <script src="https://unpkg.com/htmx.org@2.0.3"></script>
     <script src="https://cdn.tailwindcss.com"></script>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/atom-one-dark.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
+
+    <style>
+        .copy-button {
+            position: absolute;
+            top: 10px;
+            right: 10px;
+            color: #ddd;
+            cursor: pointer;
+        }
+    </style>
 </head>
 <body>
 
@@ -33,6 +43,63 @@
     </div>
 
     <script>hljs.highlightAll();</script>
+
+    <script type="text/javascript">
+        
+        document.addEventListener('DOMContentLoaded', function() {
+            var codeBlocks = document.querySelectorAll('pre > code');
+            codeBlocks.forEach(function (code) {
+                var button = document.createElement('div');
+                button.className = 'copy-button';
+                button.textContent = 'Copy';
+                var codeWrapper = code.closest('.code-block');
+                codeWrapper.appendChild(button);
+
+            })
+            initCopyCodeBlocks();
+            initCopyCodeBlocks();
+            initCopyCodeBlocks();
+        });
+
+
+        function initCopyCodeBlocks() {
+            document.removeEventListener('click', copyClickHandler);
+            document.addEventListener('click', copyClickHandler);
+        }
+
+        function copyClickHandler(event) {
+            if (event.target && event.target.classList.contains('copy-button')) {
+                var code = event.target.closest('.code-block').querySelector('code');
+                var text = code.innerText;
+                if (navigator.clipboard) {
+                  navigator.clipboard.writeText(text);
+                } else {
+                  unsecuredCopyToClipboard(text);
+                }
+                event.target.innerText = 'Copied!';
+                setTimeout(function() {
+                    event.target.innerText = 'Copy';
+                }, 2000);
+            }
+        }
+
+        // https://stackoverflow.com/questions/72237719/not-being-able-to-copy-url-to-clipboard-without-adding-the-protocol-https
+
+        function unsecuredCopyToClipboard(text) {
+          const textArea = document.createElement("textarea");
+          textArea.value = text;
+          document.body.appendChild(textArea);
+          textArea.focus();
+          textArea.select();
+          try {
+            document.execCommand('copy');
+          } catch (err) {
+            console.error('Unable to copy to clipboard', err);
+          }
+          document.body.removeChild(textArea);
+        }
+
+    </script>
 
 </body>
 </html>
--- resources/views/htmx/main.blade.php
+++ resources/views/htmx/main.blade.php
@@ -9,17 +9,23 @@
 		hx-get
 	</div>
 
+	@for ($i=0; $i<1; $i++)
+
 	<div class="font-bold mt-4">
 		Click to Load
 	</div>
 
+	<div class="code-block relative">
 	<pre><code class="language-html rounded-lg">&lt;div hx-get="/htmx/time"
 	 hx-trigger="click"&gt;
 	Click me to get current time
 &lt;/div&gt;</code></pre>
+	</div>
 
 	<div hx-get="/htmx/time"
 		 hx-trigger="click">
 		Click me to get current time
 	</div>
+
+	@endfor
 </div>
